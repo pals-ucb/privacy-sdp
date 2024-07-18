646b9237f18c8d2b5fa8fd4a2740052f00ce7ff7
Pals Chinnakannan <palsc@berkeley.edu>
2024-07-18 22:34:54 +0000

    Found the optimal weights for dp-sgd training of GPT2
    Signed-off-by: Pals Chinnakannan <palsc@berkeley.edu>

--- src/gpt2_main.py
+++ src/gpt2_main.py
@@ -9,12 +9,14 @@ import wiki2_dataloader as w2dl
 from llm_ops import llm_base_ops 
 import llm_utils
 
+
 def get_gpt2_model(model_name):
     gpt2_lm = GPT2LMHeadModel.from_pretrained(model_name)
     gpt2_tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token
     gpt2_lm.resize_token_embeddings(len(gpt2_tokenizer)) 
     return gpt2_lm, gpt2_tokenizer
+
 
 def prepare_model(rank, args, world_size, device_type):
     '''
@@ -26,52 +28,56 @@ def prepare_model(rank, args, world_size, device_type):
         rank (int): The GPU node id on which this instance is spawned.
                     if this is 0 then we are on the main instance
         world_size (int): Total number of GPU instances
-        
+
     '''
     if world_size > 1:
         print(f'Setting up DDP for rank: {rank}')
         llm_utils.ddp_setup()
     else:
-        print(f'Not setting up DDP and using either single GPU or CPU only.')
+        print('Skip DDP setup.')
 
     model_name = 'gpt2'
     llm, tokenizer = get_gpt2_model(model_name)
     train_dataloader, test_dataloader, validate_dataloader, _ = w2dl.get_wiki2_dataloaders(args, tokenizer, rank, world_size)
-    llm_ops = llm_base_ops(args, llm, tokenizer, device_type, train_dataloader, validate_dataloader,  rank, world_size)
+    llm_ops = llm_base_ops(args, llm, tokenizer, device_type, train_dataloader,
+                           validate_dataloader,  rank, world_size)
     if rank == 0 and not args.skip:
         print(llm_ops.generate('Robert went on a trip to Las Vegas, and '))
     if args.optimizer == 2:
-        layers_to_freeze = ['transformer.h.0', 'transformer.h.1', 'transformer.h.2', 'transformer.h.3', 'transformer.h.4', 
-                            'transformer.h.5', 'transformer.h.6', 'transformer.h.7', 'transformer.h.8', 'transformer.h.9',
-                            'transformer.h.10', 'transformer.h.11',
-                            'transformer.ln_f'
+        layers_to_freeze = ['transformer.h.0', 'transformer.h.1',
+                            'transformer.h.2', 'transformer.h.3',
+                            'transformer.h.4', 'transformer.h.5',
+                            'transformer.h.6', 'transformer.h.7',
+                            'transformer.h.8', 'transformer.h.9',
+                            'transformer.ln_f',
+                            'transformer.wte', 'transformer.wpe'
                             ]
         r = llm_ops.freeze_layers(layers_to_freeze)
-        print(r)
-    
+        print(f'Number of parameters           : {r[0]:>10}')
+        print(f'Number of Trainable Parameters : {r[2]:>10}')
+        print(f'Number of frozen parameters    : {r[1]:>10}')
         '''
-        l = []
-        llm_ops.get_grad_layers(l)
-        for e in l:
+        layers = []
+        llm_ops.get_grad_layers(layers)
+        for e in layers:
             print(e)
-        '''
+         '''
     llm_ops.create_optimizer()
-    llm_ops.train_loop()
-
+    llm_ops.train_start()
     if rank == 0:
         print(llm_ops.generate('Robert went on a trip to Las Vegas, and '))
-
     if world_size > 1:
         print(f'Cleaning up DDP for rank: {rank}')
         llm_utils.ddp_cleanup()
+
 
 def main(args):
     world_size, device_type = llm_utils.get_torch_device_info()
     print(f'world_size: {world_size}, device_type: {device_type}')
     if args.optimizer == 1:
-        print(f'optimizer: Stochastic Gradient Descent')
+        print('optimizer: Stochastic Gradient Descent')
     elif args.optimizer == 2:
-        print(f'optimizer: Differential Privacy Stochastic Gradient Descent')
+        print('optimizer: Differential Privacy Stochastic Gradient Descent')
     else:
         sys.exit(f'Invalid optimizer provided : {args.optimizer}')
     if world_size > 1:
@@ -80,12 +86,23 @@ def main(args):
         rank = 0
         world_size = 1
         prepare_model(rank, args, world_size, device_type)
-        
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='gpt2 llm training with optimizers SGD and DP-SGD using wiki2 dataset')
-    parser.add_argument('-e', '--epochs', type=int, default=CONST.NUM_EPOCHS, help='Total epochs to train the model')
-    parser.add_argument('-b', '--batch_size', type=int, default=CONST.BATCH_SIZE, help='Set the batch size for training.')
-    parser.add_argument('-o', '--optimizer', type=int, default=1, help='The optimizer type SGD: 1 , DP SGD{: 2')
-    parser.add_argument('-s', '--skip', action="store_true", help='Skip the initial testing of the model (before training)')
+    parser = argparse.ArgumentParser(description='gpt2 llm fine-tuning with optimizers SGD and DP-SGD using wiki2 dataset')
+    parser.add_argument('-e', '--epochs', type=int, 
+                        default=CONST.NUM_EPOCHS, 
+                        help='Total epochs to train the model')
+    parser.add_argument('-b', '--batch_size', type=int, 
+                        default=CONST.BATCH_SIZE, 
+                        help='Set the batch size for training.')
+    parser.add_argument('-l', '--epoch_log_interval', type=int, 
+                        default=CONST.EPOCH_LOG_INTERVAL, 
+                        help='Set the epoch log interval for training.')
+    parser.add_argument('-o', '--optimizer', type=int, 
+                        default=1, 
+                        help='The optimizer type SGD: 1 , DP SGD: 2')
+    parser.add_argument('-s', '--skip', action="store_true", 
+                        help='Skip the initial testing of the model (before training)')
     args = parser.parse_args()
     main(args)
--- src/llm_ops.py
+++ src/llm_ops.py
@@ -3,24 +3,26 @@
     for fine-tuning, validating, evaluating and generating text using a 
     pretrained model for baseline operations.
 '''
-import os
 import sys
 import time
+import numpy as np
 import constants as CONST
 import torch
-#from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW
-#from torch.utils.data import Dataset, DataLoader
+# from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW
+# from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
-#import s3fs
+# import s3fs
 # Imports for DP
 from opacus import PrivacyEngine
 from opacus.utils.batch_memory_manager import BatchMemoryManager
+
 
 class llm_base_ops:
     def __init__(self, args, llm, llm_tokenizer, device_type, train_dataloader, 
                  validate_dataloader, rank, world_size):
         self.args = args
         self.llm = llm
+        self.llm_original = llm
         self.llm_tokenizer = llm_tokenizer
         self.optimizer = None
         self.device_type = device_type
@@ -36,17 +38,16 @@ class llm_base_ops:
         if world_size > 2:
             print(f'llm_ops: Enabling DDP')
             self.enable_DDP()
-
-        #self.enable_device()
         return
-        
+
     def create_optimizer(self):
-        self.optimizer = torch.optim.AdamW(self.llm.parameters(), lr=CONST.LEARNING_RATE, eps=CONST.EPSILON)
+        #self.optimizer = torch.optim.AdamW(self.llm.parameters(), lr=CONST.LEARNING_RATE, eps=CONST.EPSILON)
+        self.optimizer = torch.optim.SGD(self.llm.parameters(), lr=CONST.LEARNING_RATE)
         if self.args.optimizer == 2:
             self.enable_differential_privacy()
-            
+
     def enable_differential_privacy(self):
-        self.llm.train() # put the model in training mode
+        self.llm.train()  # put the model in training mode
         self.privacy_engine = PrivacyEngine()
         self.llm, self.optimizer, self.train_dataloader = self.privacy_engine.make_private_with_epsilon(
             module=self.llm,
@@ -56,9 +57,9 @@ class llm_base_ops:
             target_delta=CONST.PRIVACY_DELTA,
             epochs=self.args.epochs, 
             max_grad_norm=CONST.MAX_GRADIENT_NORM,
-            batch_first = False
+            batch_first=False
         )
-        print(f'Enabled differential privacy')
+        print('Enabled differential privacy')
         return
 
     def enable_device(self):
@@ -82,7 +83,7 @@ class llm_base_ops:
             layers (list) : List of submodules within the llm
                            
         '''
-        total_params  = 0
+        total_params = 0
         frozen_params = 0
 
         for param in self.llm.parameters():
@@ -111,73 +112,136 @@ class llm_base_ops:
                     ret_list.append(f"Layer: {full_name}.{param_name} - requires_grad: {param.requires_grad}")
             # Recursively check the child module
             self.get_grad_layers(ret_list, child, full_name)
-    # Training function common
-    def __train(self, dataloader, description):
-        self.llm.train()
-        total_loss = 0
-        for inputs, attention_mask in tqdm(dataloader, desc=description):
-            self.optimizer.zero_grad()
-            inputs = inputs.to(self.device)
-            attention_mask = attention_mask.to(self.device)
-            outputs = self.llm(inputs, attention_mask=attention_mask, labels=inputs)
-            loss = outputs.loss
-            loss.backward()
-            self.optimizer.step()
-            total_loss += loss.item()
-        return total_loss
-        
-    # Training function
-    def train(self, dataloader):
-        loss = self.__train(dataloader, "Training model")
-        return loss/len(dataloader)
 
-    def train_dp(self, dataloader):
-        '''
-        with BatchMemoryManager(data_loader=dataloader, 
-                                max_physical_batch_size=self.args.batch_size, 
-                                optimizer=self.optimizer) as memory_safe_data_loader:
-            total_loss = self.__train(memory_safe_data_loader, "Training model with DP")
-        '''
-        with BatchMemoryManager(data_loader=dataloader, max_physical_batch_size=4, 
-                                optimizer=self.optimizer ) as memory_safe_data_loader:
-            loss = self.__train(memory_safe_data_loader, "Training model with DP")
-        epsilon, best_alpha = self.privacy_engine.get_privacy_spent()
-        print(f"Privacy spent (ε): {epsilon:.2f}")
-        return loss / len(dataloader)
-    
-    def evaluate(self, dataloader):
-        self.llm.eval()
-        total_loss = 0
-        with torch.no_grad():
-            for inputs, attention_mask in tqdm(dataloader, desc="Evaluating llm"):
+    # Training function without dp
+    def train(self, dataloader):
+        st = time.time()
+        for epoch in range(1, self.args.epochs+1):
+            if self.world_size > 1:
+                dataloader.sampler.set_epoch(epoch)
+            self.llm.train()
+            epoch_losses = []
+            for step, (inputs,
+                       attention_mask) in enumerate(tqdm(dataloader,
+                                                         desc="Training")
+                                                         ):
+                self.optimizer.zero_grad()
                 inputs = inputs.to(self.device)
                 attention_mask = attention_mask.to(self.device)
-                outputs = self.llm(inputs, attention_mask=attention_mask, labels=inputs)
+                outputs = self.llm(inputs, attention_mask=attention_mask,
+                                   labels=inputs)
                 loss = outputs.loss
-                total_loss += loss.item()
-        return total_loss / len(dataloader)
-
-    # Training loop
-    def train_loop(self):
-        self.enable_device()
-        epochs = self.args.epochs
-        st = time.time()
-        for epoch in range(1, epochs+1):
-            if self.world_size > 1:
-                print(self.train_dataloader.sampler)
-                self.train_dataloader.sampler.set_epoch(epoch)
-            if self.args.optimizer == 1:
-                train_loss = self.train(self.train_dataloader)
-            elif self.args.optimizer == 2:
-                train_loss = self.train_dp(self.train_dataloader)
-            else:
-                print(f'llm_ops: invalid type of optimizer.')
-                sys.exit(-1)
-            valid_loss = self.evaluate(self.valid_dataloader)
+                loss.backward()
+                epoch_losses.append(loss.item())
+                self.optimizer.step()
+                if step > 0 and step % self.args.epoch_log_interval == 0:
+                    train_loss = np.mean(epoch_losses)
+                    eval_loss, eval_accuracy = self.evaluate(self.validate_dataloader)
+                    print('')
+                    print(
+                        f"Epoch: {epoch} | "
+                        f"Step: {step} | "
+                        f"Train loss: {train_loss:.3f} | "
+                        f"Eval loss: {eval_loss:.3f} | "
+                        f"Eval accuracy: {eval_accuracy:.3f} "
+                        )
+            train_loss = np.mean(epoch_losses)
+            valid_loss = self.evaluate(self.validate_dataloader)
             print(f"Epoch {epoch}, Train Loss: {train_loss}, Validation Loss: {valid_loss}")
         en = time.time()
         print(f'llm_ops: training total time: {(en-st)/60} mins')
         return (st, en)
+
+    def train_dp(self, dataloader):
+        for epoch in range(1, self.args.epochs+1):
+            epoch_losses = []
+            with BatchMemoryManager(
+                data_loader=dataloader, 
+                max_physical_batch_size=CONST.BATCH_SIZE_MEM_LIMIT, 
+                optimizer=self.optimizer
+            ) as msdl:
+                for step, (inputs, 
+                           attention_mask) in enumerate(tqdm(msdl, 
+                                                             desc='DP Training'
+                                                            )
+                                                        ):
+                    self.optimizer.zero_grad()
+                    inputs = inputs.to(self.device)
+                    attention_mask = attention_mask.to(self.device)
+                    outputs = self.llm(inputs, attention_mask=attention_mask,
+                                       labels=inputs)
+                    loss = outputs.loss
+                    loss.backward()
+                    epoch_losses.append(loss.item())
+                    self.optimizer.step()
+                    if step > 0 and step % self.args.epoch_log_interval == 0:
+                        train_loss = np.mean(epoch_losses)
+                        eps = self.privacy_engine.get_epsilon(CONST.PRIVACY_DELTA)
+                        eval_loss, eval_accuracy = self.evaluate_dp(self.validate_dataloader)
+                        print(
+                          f"Epoch: {epoch} | "
+                          f"Step: {step} | "
+                          f"Train loss: {train_loss:.3f} | "
+                          f"Eval loss: {eval_loss:.3f} | "
+                          f"Eval accuracy: {eval_accuracy:.3f} | "
+                          f"ɛ: {eps:.2f}"
+                        )
+
+    def evaluate(self, dataloader):
+        def accuracy(y, y_hat):
+            return (y == y_hat).mean()
+        self.llm.eval()
+        total_loss = 0
+        loss_arr = []
+        accuracy_arr = []
+        for inputs, attention_mask in tqdm(dataloader, 
+                                           desc="Evaluating"):
+            with torch.no_grad():
+                inputs = inputs.to(self.device)
+                attention_mask = attention_mask.to(self.device)
+                outputs = self.llm(inputs,
+                                   attention_mask=attention_mask,
+                                   labels=inputs)
+                loss, logits = outputs[:2]
+                preds = np.argmax(logits.detach().cpu().numpy(), axis=2)
+                labels = inputs.detach().cpu().numpy()
+                loss_arr.append(loss.item())
+                accuracy_arr.append(accuracy(preds, labels))
+        self.llm.train()
+        return np.mean(loss_arr), np.mean(accuracy_arr) 
+
+    def evaluate_dp(self, dataloader):
+        def accuracy(y, y_hat):
+            return (y == y_hat).mean()
+        self.llm.eval()
+        loss_arr = []
+        accuracy_arr = []
+        for inputs, attention_mask in dataloader:
+            with torch.no_grad():
+                inputs = inputs.to(self.device)
+                attention_mask = attention_mask.to(self.device)
+                outputs = self.llm(inputs,
+                                   attention_mask=attention_mask,
+                                   labels=inputs)
+                loss, logits = outputs[:2]
+                preds = np.argmax(logits.detach().cpu().numpy(), axis=2)
+                labels = inputs.detach().cpu().numpy()
+                loss_arr.append(loss.item())
+                accuracy_arr.append(accuracy(preds, labels))
+        self.llm.train()
+        return np.mean(loss_arr), np.mean(accuracy_arr) 
+
+    def train_start(self):
+        print('llm_ops: starting model fine-tuning.')
+        self.enable_device()
+        if self.args.optimizer == 1:
+            self.train(self.train_dataloader)
+        elif self.args.optimizer == 2:
+            self.train_dp(self.train_dataloader)
+        else:
+            print('llm_ops: invalid type of optimizer.')
+            sys.exit(-1)
+        return
 
     def generate(self, input_text, max_length=256):
         target = torch.device("cpu")
@@ -187,12 +251,16 @@ class llm_base_ops:
         attention_mask = torch.tensor([1] * len(input_ids[0]), dtype=torch.long).unsqueeze(0).to(target)
    
         with torch.no_grad():
-            output = self.llm.generate(input_ids, attention_mask=attention_mask, max_length=max_length, 
-                                  pad_token_id=self.llm_tokenizer.eos_token_id, do_sample=True,
-                                  num_return_sequences=5,
-                                  no_repeat_ngram_size=2,
-                                  temperature=0.7, 
-                                  top_k=50, top_p=0.95)
+            output = self.llm_original.generate(input_ids,
+                                                attention_mask=attention_mask,
+                                                max_length=max_length,
+                                                pad_token_id=self.llm_tokenizer.eos_token_id,
+                                                do_sample=True,
+                                                num_return_sequences=5,
+                                                no_repeat_ngram_size=2,
+                                                temperature=0.7,
+                                                top_k=50,
+                                                top_p=0.95)
         gen_text = self.llm_tokenizer.decode(output[0], skip_special_tokens=True)
         return gen_text
         
